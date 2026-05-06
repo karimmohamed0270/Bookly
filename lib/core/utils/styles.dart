3f858ad3796ac773347df8893a97a92ef587c675
Karim Mohamed <karimmohamed0270@gmail.com>
2026-05-06 17:16:38 +0300

    add rating to book detail page

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -17,7 +17,7 @@ abstract class Styles {
   );
 
   static const textStyle30 = TextStyle(
-    fontSize: 20,
+    fontSize: 30,
     fontWeight: FontWeight.normal,
     fontFamily: kanotherfontFamily,
   );
--- lib/features/home/presentation/views/widgets/bookdetails_appbar.dart
+++ lib/features/home/presentation/views/widgets/bookdetails_appbar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class BookDetailsAppBar extends StatelessWidget {
   const BookDetailsAppBar({super.key});
@@ -7,7 +8,12 @@ class BookDetailsAppBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Row(
       children: [
-        IconButton(onPressed: () {}, icon: Icon(Icons.close)),
+        IconButton(
+          onPressed: () {
+            GoRouter.of(context).pop();
+          },
+          icon: Icon(Icons.close),
+        ),
         Spacer(),
         IconButton(onPressed: () {}, icon: Icon(Icons.shopping_cart)),
       ],
