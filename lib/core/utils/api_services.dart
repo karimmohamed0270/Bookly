5c9797aed6b5761e3eddc70b69b2bc4296752cfc
Karim Mohamed <karimmohamed0270@gmail.com>
2026-05-12 22:07:06 +0300

    create api service helper in utils

---
+++ lib/core/utils/api_services.dart
@@ -0,0 +1,13 @@
+import 'package:dio/dio.dart';
+
+class Apiservice {
+  final String baseUrl = "https://www.googleapis.com/books/v1/volumes";
+  final Dio dio;
+  Apiservice(this.dio);
+
+  // get methods
+  Future<Map<String, dynamic>> get({required String endPoint}) async {
+    final response = await dio.get('$baseUrl$endPoint');
+    return response.data;
+  }
+}
